099b3a7c4070d2ca14614da347066827364501d9
Mauricio Romero <116466045+Mauricio410@users.noreply.github.com>
2024-03-05 12:30:52 -0300

    Add files via upload

---
+++ 19. Conoce tu Horóscopo Chino/conocetuhoroscopochino.py
@@ -0,0 +1,29 @@
+n = input("Ingrese un año de nacimiento: ")
+n = int(n)
+
+while n > 1911:
+    n -= 12
+if n == 1900:
+    print("Tu Horóscopo Chino es: Rata")
+elif n == 1901:
+    print("Tu Horóscopo Chino es: Bufalo")
+elif n == 1902:
+    print("Tu Horóscopo Chino es: Tigre")
+elif n == 1903:
+    print("Tu Horóscopo Chino es: Conejo")
+elif n == 1904:
+    print("Tu Horóscopo Chino es: Dragon")
+elif n == 1905:
+    print("Tu Horóscopo Chino es: Serpiente")
+elif n == 1906:
+    print("Tu Horóscopo Chino es: Caballo")
+elif n == 1907:
+    print("Tu Horóscopo Chino es: Cabra")
+elif n == 1908:
+    print("Tu Horóscopo Chino es: Mono")
+elif n == 1909:
+    print("Tu Horóscopo Chino es: Gallo")
+elif n == 1910:
+    print("Tu Horóscopo Chino es: Perro")
+elif n == 1911:
+    print("Tu Horóscopo Chino es: Chancho")
